aefcb5aefd0b02024dcc987534cb030f93d57542
MaratBo <marat.offic2016@gmail.com>
2022-02-10 15:38:36 +0300

    v2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import datetime
 from datetime import timedelta
 from time import sleep
-from customers import access, access2, access3, access4, access5, access6, access7, access8, \
-    access9, access10, access11, access12, access13, access14, make_message
+from customers import accesses, make_message
 from dotenv import load_dotenv
 import os
 import requests
@@ -39,7 +38,6 @@ def script(session_id, key):  # возвращает список приняты
     name_group = which_cabinet[0][-1]
     name = which_cabinet[0][key].split("'")[-2]
     start_time = f'{datetime.date.today()}T00:00:00.000Z'
-    # end_time = f'{datetime.date.today()}T15:00:00.000Z'
     headers = {
         'X-Session-Id': session_id,
         'X-Authorization': TOKEN,
@@ -93,46 +91,18 @@ def message(sms, CHAT_ID):
             'text': sms
             }
     requests.post(URL, data=data)
-    print(CHAT_ID, sms)
     flag.clear()
 
 
 def user(access):
     which_cabinet.append(access)
     time = datetime.date.today().strftime('%d.%m')
-    for key in range(len(access) - 1):
+    for key in range(len(access) - 2):
         auth(access[key], key)
     # отправляем собранный текст по звонкам
     if True in flag:
         dealer_name.append(access[-1])
-        if access[-1] == 'avtotrakt':
-            CHAT_ID = "@calls_stat"
-        elif access[-1] == 'avangard':
-            CHAT_ID = '@avangard_calls'
-        elif access[-1] == 'm2o':
-            CHAT_ID = '@m2o_autoru'
-        elif access[-1] == 'petrovsky':
-            CHAT_ID = '@petrovsky_calls'
-        elif access[-1] == 'axis':
-            CHAT_ID = '@axis_bets'
-        elif access[-1] == 'chelyabinsk':
-            CHAT_ID = access[-2]
-        elif access[-1] == 'mias':
-            CHAT_ID = '@chl_autoru'
-        elif access[-1] == 'july':
-            CHAT_ID = '@july_autoru'
-        elif access[-1] == 'skoda_planeta':
-            CHAT_ID = '@skoda_planeta'
-        elif access[-1] == 'geely_planeta':
-            CHAT_ID = '@planeta_auto_ug'
-        elif access[-1] == 'planeta_used':
-            CHAT_ID = '@planetaugprobeg'
-        elif access[-1] == 'partner_auto':
-            CHAT_ID = '@autopartnertmn'
-        elif access[-1] == 'avtograd':
-            CHAT_ID = '@agradtmn'
-        else:
-            CHAT_ID = '@avtoforum_krs'
+        CHAT_ID = access[-2]
         message(f'Звонки за {time} (всего/пропущ.)\n'
                 f'{calls_text}', CHAT_ID)
         dealer_name.clear()
@@ -151,58 +121,11 @@ def user(access):
             text = f'{text} @Danil_Bashkin'
             message(text, CHAT_ID)
             rest_money.clear()
-        elif access[-1] == 'avangard':
-            CHAT_ID = '@avangard_calls'
+        else:
+            CHAT_ID = access[-2]
             message(text, CHAT_ID)
             rest_money.clear()
-        elif access[-1] == 'petrovsky':
-            CHAT_ID = '@petrovsky_calls'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'm2o':
-            CHAT_ID = '@m2o_autoru'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'axis':
-            CHAT_ID = '@axis_bets'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'avtorum':
-            CHAT_ID = '@avtoforum_krs'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'chelyabinsk':
-            CHAT_ID = '@reginas_autoru'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'july':
-            CHAT_ID = '@july_autoru'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'mias':
-            CHAT_ID = '@chl_autoru'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'skoda_planeta':
-            CHAT_ID = '@skoda_planeta'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'geely_planeta':
-            CHAT_ID = '@planeta_auto_ug'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'planeta_used':
-            CHAT_ID = '@planetaugprobeg'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'partner_auto':
-            CHAT_ID = '@autopartnertmn'
-            message(text, CHAT_ID)
-            rest_money.clear()
-        elif access[-1] == 'avtograd':
-            CHAT_ID = '@agradtmn'
-            message(text, CHAT_ID)
-            rest_money.clear()
+
     values = ''
     if len(my_ex) > 0:
         print(my_ex)
@@ -210,103 +133,24 @@ def user(access):
             if i is not None:
                 values += f'{i}\n'
             text = f'Ваш автомобиль снова в продаже:\n{values}'
-            #my_ex.clear()
-            if access[-1] == 'chelyabinsk':
-                CHAT_ID = '@reginas_autoru'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'july':
-                CHAT_ID = '@july_autoru'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'mias':
-                CHAT_ID = '@chl_autoru'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'avtorum':
-                CHAT_ID = '@avtoforum_krs'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'skoda_planeta':
-                CHAT_ID = '@skoda_planeta'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'geely_planeta':
-                CHAT_ID = '@planeta_auto_ug'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'planeta_used':
-                CHAT_ID = '@planetaugprobeg'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'partner_auto':
-                CHAT_ID = '@autopartnertmn'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'avtograd':
-                CHAT_ID = '@agradtmn'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'axis':
-                CHAT_ID = '@axis_bets'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'avtotrakt':
-                CHAT_ID = '@calls_stat'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'avangard':
-                CHAT_ID = '@avangard_calls'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'petrovsky':
-                CHAT_ID = '@petrovsky_calls'
-                message(text, CHAT_ID)
-                my_ex.clear()
-            elif access[-1] == 'm2o':
-                CHAT_ID = '@m2o_autoru'
-                message(text, CHAT_ID)
-                my_ex.clear()
+            CHAT_ID = access[-2]
+            message(text, CHAT_ID)
+            my_ex.clear()
     which_cabinet.clear()
 
 
 if __name__ == '__main__':
     while True:
-        time_now = datetime.datetime.now()  # + timedelta(hours=3)  # смещение на американском сервере + 3ч
+        time_now = datetime.datetime.now() + timedelta(hours=3)  # смещение на американском сервере + 3ч
         h = time_now.hour
         m = time_now.minute
         d = time_now.date().strftime("%d")
         print(f'check time {h}:{m}')
         if m in range(0, 59) and h == 18:
             print(f'start script {d}-{h}:{m}')
-            # user(access)
-            # which_cabinet.clear()
-            # user(access2)
-            # which_cabinet.clear()
-            # user(access3)
-            # which_cabinet.clear()
-            # user(access4)
-            # which_cabinet.clear()
-            # user(access5)
-            # which_cabinet.clear()
-            user(access6)
-            which_cabinet.clear()
-            user(access7)
-            which_cabinet.clear()
-            user(access8)
-            which_cabinet.clear()
-            user(access9)
-            which_cabinet.clear()
-            user(access10)
-            which_cabinet.clear()
-            # user(access11)
-            # which_cabinet.clear()
-            # user(access12)
-            # which_cabinet.clear()
-            # user(access13)
-            # which_cabinet.clear()
-            # user(access14)
-            # which_cabinet.clear()
+            for i in accesses:
+                user(i)
+                which_cabinet.clear()
             sleep(84600)
         else:
             sleep(1200)
